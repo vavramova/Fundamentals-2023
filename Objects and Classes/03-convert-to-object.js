ae894de120f55cd1ec5cb16b0d7681d5aaaf2325
Viktoria <avramovaviki017@gmail.com>
2023-11-05 17:25:49 +0200

    add

--- Objects and Classes/03-convert-to-object.js
+++ Objects and Classes/03-convert-to-object.js
@@ -4,7 +4,7 @@ function convertToObject(jsonStr){
     let entries = Object.entries(person);
     
     for(let entry of entries){
-        console.log(entry[0],':', entry[1]);
+        console.log(`${entry[0]}: ${entry[1]}`);
     }
 }
 
---
+++ Objects and Classes/04-convert-to-json.js
@@ -0,0 +1,8 @@
+function convertToJSON(firstName, lastName, hairColor){
+    let person = { name: firstName, lastName: lastName, hairColor: hairColor};
+
+    console.log(JSON.stringify(person));
+}
+convertToJSON('George', 'Jones',
+
+'Brown')
