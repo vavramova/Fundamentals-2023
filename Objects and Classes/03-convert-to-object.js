544a671b3b9dc6b3018aa9c1aaca1eba8edf086e
Viktoria <avramovaviki017@gmail.com>
2023-11-05 17:19:17 +0200

    new add

---
+++ Objects and Classes/03-convert-to-object.js
@@ -0,0 +1,11 @@
+function convertToObject(jsonStr){
+    let person = JSON.parse(jsonStr);
+
+    let entries = Object.entries(person);
+    
+    for(let entry of entries){
+        console.log(entry[0],':', entry[1]);
+    }
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}')
