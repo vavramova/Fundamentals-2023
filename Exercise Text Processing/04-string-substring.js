adce7dd4ec466426bdcbad4d20efc703d98ce1c8
Viktoria <avramovaviki017@gmail.com>
2023-11-13 22:50:14 +0200

    adding ex4

---
+++ Exercise Text Processing/04-string-substring.js
@@ -0,0 +1,13 @@
+function stringSubstring(word, text){
+    text = text.toLowerCase().split(' ');
+
+    if(text.includes(word)){
+        console.log(word);
+    }else {
+        console.log(`${word} not found.`);
+    }
+}
+
+stringSubstring('javascript',
+
+'JavaScript is the best programming language')
