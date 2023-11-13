6abe532ef23f86a69a236d8c0eacecc95e94fc20
Viktoria <avramovaviki017@gmail.com>
2023-11-13 23:23:28 +0200

    adding ex5

---
+++ Exercise Text Processing/05-replace-repeating-chars.js
@@ -0,0 +1,15 @@
+function replaceRepeatingChars(str){
+    let noRepeatStr = '';
+    let prevChar = '';
+
+    for( let curChar of str){
+        if(curChar !== prevChar){
+            noRepeatStr += curChar;
+            prevChar = curChar;
+        }
+    }
+
+    console.log(noRepeatStr);
+}
+
+replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa')
