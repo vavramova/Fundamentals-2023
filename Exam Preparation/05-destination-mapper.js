267fb761dab71ebef57b04ed924fac4a30dbcbd2
Viktoria <avramovaviki017@gmail.com>
2023-12-01 02:28:05 +0200

    adding ex5 with potential mistake

---
+++ Exam Preparation/05-destination-mapper.js
@@ -0,0 +1,18 @@
+function destinationMapper(str){
+    let pattern = /([=|\/])(?<destination>[A-Z][A-Za-z]{3,})\1/g;
+    let matches = str.matchAll(pattern);
+    let destinations = [];
+
+    for(let match of matches){
+        let validDest = match.groups.destination;
+        destinations.push(validDest);
+    }
+
+    let travelPoints = 0;
+    destinations.forEach(destination => travelPoints += destination.length);
+
+    console.log(`Destinations: ${destinations.join(', ')}`);
+    console.log(`Travel Points: ${travelPoints}`);
+}
+
+destinationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=");
